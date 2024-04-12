25b0d57b8f1bd7449de60b435b2b48d67302cba6
danielolivermoco <danielmoco10@gmail.com>
2024-04-11 21:38:04 -0300

    alteração responsividade

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ const links = document.querySelectorAll('.navbar-links a li');
 
 menuList.addEventListener('click', () => {
     nav.classList.toggle('active');
-  });
+});
   
-  links.forEach(item => {
+links.forEach(item => {
     item.addEventListener('click', () => {
       nav.classList.toggle('active');
-    })
   })
+})
